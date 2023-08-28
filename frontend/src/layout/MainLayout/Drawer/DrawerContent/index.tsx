c2e00d6c2f647fda8001038016e2465a725e38bc
Anik Sarker <sarkerjr@gmail.com>
2023-08-29 01:10:57 +0600

    refactor - sidebar populated with some design change

--- frontend/src/layout/MainLayout/Drawer/DrawerContent/index.tsx
+++ frontend/src/layout/MainLayout/Drawer/DrawerContent/index.tsx
@@ -3,16 +3,16 @@ import Navigation from './Navigation';
 import SimpleBar from '@/components/SimpleBar';
 
 const DrawerContent = () => (
-  <SimpleBar
-    sx={{
-      '& .simplebar-content': {
-        display: 'flex',
-        flexDirection: 'column',
-      },
-    }}
-  >
-    <Navigation />
-  </SimpleBar>
+  // <SimpleBar
+  //   sx={{
+  //     '& .simplebar-content': {
+  //       display: 'flex',
+  //       flexDirection: 'column',
+  //     },
+  //   }}
+  // >
+  <Navigation />
+  // </SimpleBar>
 );
 
 export default DrawerContent;
