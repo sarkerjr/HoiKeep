49e60d1e5331938fe03dcd6a6d7f322541112103
Anik Sarker <sarkerjr@gmail.com>
2023-09-21 15:10:19 +0600

    refactor - DatePicker component validation added

--- frontend/src/components/DatePicker.tsx
+++ frontend/src/components/DatePicker.tsx
@@ -1,12 +1,30 @@
+import { useState, useMemo } from 'react';
 import dayjs from 'dayjs';
-import { TextField } from '@mui/material';
+
 import {
   LocalizationProvider,
   DatePicker as MuiDatePicker,
 } from '@mui/x-date-pickers';
+import { DateValidationError } from '@mui/x-date-pickers/models';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const DatePicker = (props) => {
+  const [error, setError] = useState<DateValidationError | null>(null);
+
+  const errorMessage = useMemo(() => {
+    switch (error) {
+      case 'maxDate': {
+        return props.errorMessage.maxDate;
+      }
+      case 'minDate': {
+        return props.errorMessage.minDate;
+      }
+      default: {
+        return '';
+      }
+    }
+  }, [error]);
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <MuiDatePicker
@@ -14,10 +32,15 @@ const DatePicker = (props) => {
         sx={props.sx}
         label={props.label}
         value={dayjs(props.value)}
-        minDate={props.minDate ? dayjs(props.minDate) : null}
         openTo={props?.openTo}
         onChange={(newValue) => {
           props.onChange(newValue ? dayjs(newValue as Date) : dayjs());
+        }}
+        onError={(newError) => setError(newError)}
+        slotProps={{
+          textField: {
+            helperText: errorMessage,
+          },
         }}
       />
     </LocalizationProvider>
